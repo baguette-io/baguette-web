93101563f2f2fd9f47ea9c385e659b48c8a52602
David Macro <david.macro7@gmail.com>
2017-08-11 00:06:22 +0200

    Add a default admin layout

---
+++ components/adminnavbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar navbar-toggleable-md navbar-light">
+	    <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="/images/favicon.png" class="d-inline-block" width="50" height="50"> baguette.io
+            </a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav navbar-right">
+                    <li class="">
+                        <a class="btn btn-danger" href="/logout" role="button">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <admin-navbar />
+    <nuxt/>
+    <default-footer />
+  </div>
+</template>
+
+<script>
+import AdminNavbar from '../components/adminnavbar'
+import DefaultFooter from '../components/defaultfooter'
+export default {
+  layout: 'default',
+  components: {
+    AdminNavbar,
+    DefaultFooter
+  }
+}
+</script>
