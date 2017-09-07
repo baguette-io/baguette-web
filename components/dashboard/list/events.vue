401e46eb4d8905e7463cacd012d6528edda622c3
David Macro <david.macro7@gmail.com>
2017-09-07 11:55:27 +0200

    extract the events listing to its own component

---
+++ components/dashboard/list/events.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <small class="text-danger text-uppercase font-weight-normal">5 minutes ago</small>
+                <div class="row">
+                    <div class="col">
+                        <img src="/images/avatar.svg" class="rounded-circle avatar" alt="avatar" />
+                        <span class="font-weight-bold">baguette.io</span>&nbsp;fail to deploy on&nbsp;<small class="text-muted text-uppercase text-weight-light">staging</small>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <small class="text-muted text-uppercase font-weight-normal">25 minutes ago</small>
+                <div class="row">
+                    <div class="col">
+                        <img src="/images/avatar.svg" class="rounded-circle avatar" alt="avatar" />
+                        <span class="font-weight-bold">David</span>&nbsp;deploy&nbsp;<span class="font-weight-bold">baguette.io</span>&nbsp;on&nbsp;<small class="text-muted text-uppercase text-weight-light">staging</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <small class="text-muted text-uppercase font-weight-normal">1 hour ago</small>
+                <div class="row">
+                    <div class="col">
+                        <img src="/images/avatar.svg" class="rounded-circle avatar" alt="avatar" />
+                        <span class="font-weight-bold">Charles</span>&nbsp;added David to&nbsp;<span class="font-weight-bold">baguette.io</span>&nbsp;<small class="text-muted text-uppercase text-weight-light">organization</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <small class="text-success text-uppercase font-weight-normal">2 hour ago</small>
+                <div class="row">
+                    <div class="col">
+                        <img src="/images/avatar.svg" class="rounded-circle avatar" alt="avatar" />
+                        <span class="font-weight-bold">baguette.io</span>&nbsp;success to deploy on&nbsp;<small class="text-muted text-uppercase text-weight-light">master</small>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
