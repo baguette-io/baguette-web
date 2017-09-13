bce2a203d51690e810d80ffc4812db896ee4c10c
David Macro <david.macro7@gmail.com>
2017-09-13 16:45:31 +0200

    /organizations/: add a link to the organization detail

--- components/dashboard/list/organizations.vue
+++ components/dashboard/list/organizations.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="list-group">
-        <div class="list-group-item list-group-item-action flex-column align-items-start" v-for="obj in objects.results">
+        <nuxt-link v-bind:to="'/dashboard/organizations/' + obj.organization.name + '/'" :key="obj.organization.name" class="list-group-item list-group-item-action flex-column align-items-start" v-for="obj in objects.results">
             <div class="d-flex justify-content-between">
                 <h5 class="mb-1">{{ obj.organization.name }}&nbsp;<small class="text-muted">{{ obj.organization.description }}</small></h5>
                 <small class="text-muted">Added on {{ obj.organization.date_created | format_date }}</small>
@@ -26,12 +26,12 @@
                     </div>
                 </div>
                 <div class="col-2">
-                    <button class="btn btn-block btn-danger" role="button" v-if="obj.is_owner" :disabled="!obj.organization.deletable" @click="$emit('show-delete-organization', obj.organization.name)">
+                    <button class="btn btn-block btn-danger" role="button" v-if="obj.is_owner" :disabled="!obj.organization.deletable" @click.prevent="$emit('show-delete-organization', obj.organization.name)">
                         Delete
                     </button>
                 </div>
             </div>
-        </div>
+        </nuxt-link>
     </div>
 </template>
 
