c48ce42bf6dbe747193766115ca2dafaa0be0b07
David Macro <david.macro7@gmail.com>
2017-09-21 13:30:16 +0200

    logout works.

---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="min-height:500px;">
+        <div class="container">
+            <div class="row">
+                Logout...
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  mounted: function (redirect) {
+    this.$store.commit('logout')
+    this.$router.replace('/login')
+  }
+}
+</script>
