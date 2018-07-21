88317bbc7da1da4c6329dde13f6ababd74432b6b
David Macro <david.macro@volvocars.com>
2018-07-21 22:46:05 +0200

    login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,64 +1,59 @@
 <template>
-    <div class="container" style="min-height: 500px;">
-        <div class="row">
-            <div class="col-md-5"></div>
-            <div class="col-md-4">
-                <h2 class="title">Log in</h2>
+    <div class="container headtitle">
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-auto">
+                <h1>Log in</h1>
             </div>
         </div>
-        <form class="form-horizontal" method="post" v-on:submit.prevent="login" action="">
-            <div class="form-group row" v-bind:class="{ 'has-danger': generalError }">
-                <template v-if="generalError">
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-auto">
+                <h6>Let's bake</h6>
+            </div>
+        </div>
+        <div class="justify-content-md-center">
+            <form class="container mt-4" novalidate method="post" v-on:submit.prevent="login" action="">
+                <div class="row form-group">
                     <div class="col-md-4"></div>
-                    <label for="error" class="sr-only">Error</label>
                     <div class="col-md-4">
-                        <div class="text-center text-danger">
-                        {{ generalError }}
+                        <label for="username" class="sr-only">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text bg-fr-blue border-fr-blue text-white"><i class="fa fa-user fa-fw"></i></div>
+                            </div>
+                            <input type="text" name="username" v-model="username" class="form-control" placeholder="Username" required="" />
                         </div>
                     </div>
-                </template>
-            </div>
-            <div class="form-group row" v-bind:class="{ 'has-danger': usernameError }">
-                <div class="col-md-4"></div>
-                <label for="username" class="sr-only">Username</label>
-                <div class="input-group col-md-4">
-                    <div class="input-group-addon"><i class="fa fa-user fa-fw"></i></div>
-                    <input type="text" name="username" v-model="username" class="form-control" placeholder="Username" required="" />
-                </div>
-                <div class="col-md-4">
-                    <div class="form-control-feedback">
-                        <template v-if="usernameError">
-                            <span class="text-danger align-middle">
-                                <i class="fa fa-close">{{ usernameError }}</i>
-                            </span>
-                        </template>
+                    <div class="col-md-4">
+                        <div class="validation-error" v-bind:class="{ 'visible': generalError }">
+                            {{ generalError }}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="form-group row" v-bind:class="{ 'has-danger': passwordError }">
-                <div class="col-md-4"></div>
-                <label for="password" class="sr-only">Password</label>
-                <div class="input-group col-md-4">
-                    <div class="input-group-addon"><i class="fa fa-key fa-fw"></i></div>
-                    <input type="password" name="password" v-model="password" class="form-control" placeholder="P@ssw0rd!" required="" />
-                </div>
-                <div class="col-md-4">
-                    <div class="form-control-feedback">
-                        <template v-if="passwordError">
-                            <span class="text-danger align-middle">
-                                <i class="fa fa-close">{{ passwordError }}</i>
-                            </span>
-                        </template>
+                <div class="row form-group">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <label for="password" class="sr-only">Password</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text bg-fr-white"><i class="fa fa-key fa-fw"></i></div>
+                            </div>
+                            <input type="password" name="password" v-model="password" class="form-control" placeholder="P@ssw0rd!" required="" />
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="validation-error" v-bind:class="{ 'visible': passwordError }">
+                            {{ passwordError }}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4">
-                    <input type="submit" class="btn btn-block btn-outline-danger" value="Log in" />
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <input type="submit" class="btn btn-block btn-fr-red" value="Log in" />
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 
